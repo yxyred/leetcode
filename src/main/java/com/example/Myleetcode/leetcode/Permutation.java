f7b0ba1faf5fdf9917e91592feb7a3ca91a25d22
yxy <731581233@qq.com>
2018-06-05 16:30:06 +0800

    全排列

--- src/main/java/com/example/Myleetcode/leetcode/Permutation.java
+++ src/main/java/com/example/Myleetcode/leetcode/Permutation.java
@@ -2,18 +2,23 @@ package com.example.Myleetcode.leetcode;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
+
 /*
-*
+*全排列
 * */
 @Slf4j
 public class Permutation {
-    public static void main(String[] args) {
-        String inter = "abcd";
-        log.info(new Permutation().Permutation_no(inter).toString());
-        System.out.println(new Permutation().Permutation_no(inter).toString());
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList res = new Permutation().Permutation_no(scanner.next());
+        log.info(res.toString());
+        log.info("count:{}",res.size());
+        scanner.close();
     }
     public ArrayList Permutation_no(String inter){
         List<String> res = new ArrayList<>();
@@ -23,7 +28,7 @@ public class Permutation {
     }
     public void PermutationHelper(char[] cs , int i, List<String> list){
         if(i == cs.length-1){
-            String val = cs.toString();
+            String val = String.valueOf(cs);
             if(!list.contains(val)){
                 list.add(val);
             }
@@ -34,7 +39,6 @@ public class Permutation {
                 swap(cs,i,j);
             }
         }
-
     }
     public void swap(char[] chars,int i ,int j){
         char temp = chars[i];
